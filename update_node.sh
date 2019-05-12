a303e705f316f6cb42cd05ec84899c8222a79925
cryptonodes-core <38521753+cryptonodes-core@users.noreply.github.com>
2019-05-12 11:59:22 +0200

    updated for version 1.4.4

--- update_node.sh
+++ update_node.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-TARBALLURL="https://github.com/cryptonodes-core/cryptonodes-core/releases/download/v1.4.3/cryptonodes-1.4.3-x86_64-linux-gnu.tar.gz"
-TARBALLNAME="cryptonodes-1.4.3-x86_64-linux-gnu.tar.gz"
-CNMCVERSION="1.4.3"
+TARBALLURL="https://github.com/cryptonodes-core/cryptonodes-core/releases/download/v1.4.4/cryptonodes-1.4.4-x86_64-linux-gnu.tar.gz"
+TARBALLNAME="cryptonodes-1.4.4-x86_64-linux-gnu.tar.gz"
+CNMCVERSION="1.4.4"
 
 CHARS="/-\|"
 
